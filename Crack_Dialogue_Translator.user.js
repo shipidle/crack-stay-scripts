edaa131954120420b24778ee51be218b44935efd
shipidle <guswl3456@naver.com>
2026-07-27 17:17:37 +0900

    Update Crack_Dialogue_Translator.user.js

--- Crack_Dialogue_Translator.user.js
+++ Crack_Dialogue_Translator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         🌐 대사 영문 번역기
 // @namespace    https://github.com/shipidle/crack-stay-scripts/crack-dialogue-translator
-// @version      0.1.5
+// @version      0.1.6
 // @description  🧪 BETA · 크랙 채팅 입력문의 한국어 대사만 영문으로 번역하고 원문 대사를 함께 보존합니다.
 // @icon         data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%2064%2064%22%3E%3Ctext%20x=%220%22%20y=%2252%22%20font-size=%2252%22%3E%F0%9F%8C%8A%3C/text%3E%3C/svg%3E
 // @author       shipidle
